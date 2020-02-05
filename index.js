e7284205302eab7dba53c8f2a437f281580c53b7
daniel.gomez <daniel.gomez@techedgegroup.com>
2020-02-06 00:01:35 +0100

    refreshed token

--- index.js
+++ index.js
@@ -28,4 +28,4 @@ client.on('message', msg => {
     // console.log("Mensaje en canal", msg.channel)
 });
 
-client.login('Njc0NjY0Mzk4OTE2Mjg4NTE3.XjsrWw.gMM8DCa5wG0eXerdtY5Ma1VGuB4');
+client.login('Njc0NjY0Mzk4OTE2Mjg4NTE3.XjtJPQ.Z_NdwF99dbrW-sHkFgFs3cYU8mE');
