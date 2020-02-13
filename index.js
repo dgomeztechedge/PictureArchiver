d429cf3dd619415c9c25f793d099eaf07fea0d97
HachebeDLC <hbgam3r@hotmail.es>
2020-02-13 16:51:32 +0100

    Archive EVERYTHING

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ imgur.setClientId(process.env.client_id);
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
+    client.user.setActivity("Saving pictures");
+    client.user.setPresence({
+        game: {
+            name: 'https://poalof-media.netlify.com/',
+            type: 'PLAYING',
+        }
+    })
 
 });
 
@@ -47,7 +54,27 @@ client.on('message', msg => {
         msg.channel.send(attachment);
     }
 });
-
+client.on('raw', packet => {
+    // We don't want this to run on unrelated packets
+    if (packet.t === 'MESSAGE_REACTION_ADD') {
+        // Grab the channel to check the message from
+        const channel = client.channels.get(packet.d.channel_id);
+        // There's no need to emit if the message is cached, because the event will fire anyway for that
+        if (channel.messages.has(packet.d.message_id)) return;
+        // Since we have confirmed the message is not cached, let's fetch it
+        channel.fetchMessage(packet.d.message_id).then(message => {
+            // Emojis can have identifiers of name:id format, so we have to account for that case as well
+            const emoji = packet.d.emoji.id ? `${packet.d.emoji.name}:${packet.d.emoji.id}` : packet.d.emoji.name;
+            // This gives us the reaction we need to emit the event properly, in top of the message object
+            const reaction = message.reactions.get(emoji);
+            // Adds the currently reacting user to the reaction's users collection.
+            if (reaction) reaction.users.set(packet.d.user_id, client.users.get(packet.d.user_id));
+            // Check which type of event it is before emitting
+                client.emit('messageReactionAdd', reaction, client.users.get(packet.d.user_id));
+            });
+            
+        } else return;
+});
 client.on('messageUpdate', (oldMsg, newMsg) => {
     if (newMsg.channel.name === 'portrait-media' && !newMsg.author.bot) {
         if (Array.isArray(newMsg.embeds)) {
@@ -82,7 +109,7 @@ client.on('messageReactionAdd', (reaction, user) => {
         if (reaction.message.attachments.array().length > 0) {
             exampleEmbed.setImage(reaction.message.attachments.array()[0].proxyURL)
         }
-        if(reaction.message.embeds > 0 && reaction.message.embeds[0].image){
+        if (reaction.message.embeds > 0 && reaction.message.embeds[0].image) {
             exampleEmbed.setImage(reaction.message.embeds[0].image.url)
         }
         user.send(exampleEmbed)
